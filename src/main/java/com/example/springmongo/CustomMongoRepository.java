c95faa12c3db74c68c2bd6794b735aafd2225d2d
ForexPro <roma.minihanowa@gmail.com>
2023-11-22 16:19:00 +0400

    Вынес сохранение uuid из metadata

--- src/main/java/com/example/springmongo/CustomMongoRepository.java
+++ src/main/java/com/example/springmongo/CustomMongoRepository.java
@@ -1,9 +1,14 @@
 package com.example.springmongo;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
 import com.mongodb.client.gridfs.model.GridFSFile;
+import com.mongodb.client.gridfs.model.GridFSUploadOptions;
+import org.bson.BsonBinary;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
@@ -26,35 +31,54 @@ public class CustomMongoRepository<T extends OnSaveData> {
     @Autowired
     private GridFsTemplate gridFsTemplate;
 
+    @Autowired
+    private MongoDatabaseFactory dbFactory;
+
+    private GridFSBucket getGridFs() {
+        MongoDatabase db = this.dbFactory.getMongoDatabase();
+        return GridFSBuckets.create(db);
+    }
+
 
     public void saveAll(Collection<T> entities) {
         entities.parallelStream().forEach(this::save);
     }
 
+//    public void save(T entity) {
+//        DBObject metaData = new BasicDBObject();
+//        metaData.put("_id", entity.getUuid());
+//        metaData.put("hashcode", entity.getHashCode());
+//        try (InputStream stream = new ByteArrayInputStream(SerializationUtils.serialize(entity))) {
+//            gridFsTemplate.store(stream, entity.getUuid().toString(), metaData);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
     public void save(T entity) {
-        DBObject metaData = new BasicDBObject();
-        metaData.put("_id", entity.getUuid());
-        metaData.put("hashcode", entity.getHashCode());
         try (InputStream stream = new ByteArrayInputStream(SerializationUtils.serialize(entity))) {
-            gridFsTemplate.store(stream, entity.getUuid().toString(), metaData);
+            GridFSUploadOptions options = new GridFSUploadOptions()
+                    .chunkSizeBytes(261120)
+                    .metadata(new Document("hashcode", entity.getHashCode()));
+            getGridFs().uploadFromStream(new BsonBinary(entity.getUuid()), entity.getUuid().toString(), stream, options);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     public Optional<T> findById(UUID uuid) {
-        GridFSFile gridFsFile = gridFsTemplate.findOne(new Query(Criteria.where("metadata._id").is(uuid)));
+        GridFSFile gridFsFile = gridFsTemplate.findOne(new Query(Criteria.where("_id").is(uuid)));
         return Optional.of(deserialize(gridFsFile));
     }
 
     public List<T> findAll(List<UUID> uuids) {
         List<GridFSFile> gridFSFiles = new ArrayList<>();
-        gridFsTemplate.find(new Query(Criteria.where("metadata._id").in(uuids))).into(gridFSFiles);
+        gridFsTemplate.find(new Query(Criteria.where("_id").in(uuids))).into(gridFSFiles);
         return gridFSFiles.stream().parallel().map(this::deserialize).collect(Collectors.toList());
     }
 
     public void deleteAll(List<UUID> uuids) {
-        gridFsTemplate.delete(new Query(Criteria.where("metadata._id").nin(uuids)));
+        gridFsTemplate.delete(new Query(Criteria.where("_id").nin(uuids)));
     }
 
     private T deserialize(GridFSFile file) {
