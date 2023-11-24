33ae913941c0d57b485e13f8eb6411890e11c890
ForexPro <roma.minihanowa@gmail.com>
2023-11-24 11:54:43 +0400

    rework from side brunch

--- src/main/java/com/example/springmongo/ConvertController.java
+++ src/main/java/com/example/springmongo/ConvertController.java
@@ -3,7 +3,7 @@ package com.example.springmongo;
 import com.example.springmongo.geometrydata.CustomMongoRepository;
 import com.example.springmongo.geometrydata.GeometryData;
 import com.example.springmongo.geometrydata.OnSaveData;
-import com.example.springmongo.geometrydata.PGeometryDataRepository;
+import com.example.springmongo.geometrydata.GeometryDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,27 +12,23 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
-import java.util.List;
 import java.util.Random;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin
-public class ConverContoller {
+public class ConvertController {
 
-    private static final Logger log = LoggerFactory.getLogger(ConverContoller.class);
+    private static final Logger log = LoggerFactory.getLogger(ConvertController.class);
 
     @Autowired
-    private PGeometryDataRepository pgeometryRepository;
+    private GeometryDataRepository geometryRepository;
 
     @Autowired
     private MongoTemplate mongoTemplate;
@@ -51,7 +47,7 @@ public class ConverContoller {
     @GetMapping("/{batchSize}")
     public String batchConvertDB(@PathVariable int batchSize) {
         var dateToConvert = new Timestamp(new Date().getTime());
-        var size = pgeometryRepository.countAllByCreateDateBeforeAndConvertedFalse(dateToConvert);
+        var size = geometryRepository.countAllByCreateDateBeforeAndConvertedFalse(dateToConvert);
         log.info("Date to find converted rows : " + dateToConvert);
 
 
@@ -71,7 +67,7 @@ public class ConverContoller {
 
     }
 
-    public boolean pullBatch(int batchSize, Timestamp dateToConvert, int currentChunk, int chunks) {
+    private boolean pullBatch(int batchSize, Timestamp dateToConvert, int currentChunk, int chunks) {
         try {
             Thread.sleep(new Random().nextInt(10) + 15);
         } catch (InterruptedException e) {
@@ -80,7 +76,7 @@ public class ConverContoller {
         try {
             log.info("STEP: " + currentChunk + "/" + chunks);
             Instant start = Instant.now();
-            var page = pgeometryRepository.findAllByCreateDateBeforeAndConvertedFalse(dateToConvert, PageRequest.of(0, batchSize)).toList();
+            var page = geometryRepository.findAllByCreateDateBeforeAndConvertedFalse(dateToConvert, PageRequest.of(0, batchSize)).toList();
 
             log.info("1) Get from base " + Duration.between(start, start = Instant.now()).toMillis() + " ms; List size: " + page.size());
 
@@ -92,7 +88,7 @@ public class ConverContoller {
 
             log.info("2) Saved in mongodb " + Duration.between(start, start = Instant.now()).toMillis() + " ms");
 
-            pgeometryRepository.markAsConverted(page.stream().parallel().map(GeometryData::getUuid).collect(Collectors.toList()));
+            geometryRepository.markAsConverted(page.stream().parallel().map(GeometryData::getUuid).collect(Collectors.toList()));
 
             log.info("3) Patch postgres " + Duration.between(start, Instant.now()).toMillis() + " ms");
 
--- src/main/java/com/example/springmongo/geometrydata/GeometryDataRepository.java
+++ src/main/java/com/example/springmongo/geometrydata/GeometryDataRepository.java
@@ -1,10 +1,28 @@
 package com.example.springmongo.geometrydata;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface GeometryDataRepository extends MongoRepository<MongoGeometryDataBin, UUID> {
+public interface GeometryDataRepository extends PagingAndSortingRepository<GeometryData, UUID> {
+
+    Page<GeometryData> findAllByCreateDateBeforeAndConvertedFalse(Timestamp date, Pageable pageable);
+
+    Long countAllByCreateDateBeforeAndConvertedFalse(Timestamp date);
+
+    @Transactional
+    @Modifying(clearAutomatically=true, flushAutomatically=true)
+    @Query(value = "update geometry_data set converted = true where uuid in :uuids", nativeQuery = true)
+    void markAsConverted(@Param("uuids") List<UUID> uuids);
+
 }
